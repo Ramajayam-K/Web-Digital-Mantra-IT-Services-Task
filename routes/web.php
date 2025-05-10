c35c354d53473282a14954ef341e5fcd5c51a7c8
Ramajayam-K <ramajayamkasi11@gmail.com>
2025-05-10 13:47:13 +0530

    Task 3 Add API Token Authentication is completed

--- routes/web.php
+++ routes/web.php
@@ -15,19 +15,21 @@ Route::get('dashboard', function () {
 // Task 1 : Create a RESTful API for a Blog
 
 // Task 2 : Implement API Rate Limiting
-
-Route::controller(BlogsController::class)->group(function(){
-    Route::post('/showPostsComments','showPostsComments')->name('showPostsComments')->middleware('throttle:200,1');
-    Route::middleware('throttle:blogs')->group(function(){
-        Route::get('/blogs','index')->name('blogs');
-        Route::post('/createPost','createPost')->name('createPost');
-        Route::post('/createComments','createComments')->name('createComments');
-        Route::put('/updatePost','updatePost')->name('updatePost');
-        Route::put('/updateComments','index')->name('updateComments');
-        Route::delete('/deletePost','deletePost')->name('deletePost');
-        Route::delete('/deleteComments','deleteComments')->name('deleteComments'); 
-    }); 
+Route::middleware(['auth.sanctum'])->group(function(){
+    Route::controller(BlogsController::class)->group(function(){
+        Route::post('/showPostsComments','showPostsComments')->name('showPostsComments')->middleware('throttle:200,1');
+        Route::middleware('throttle:blogs')->group(function(){
+            Route::get('/blogs','index')->name('blogs');
+            Route::post('/createPost','createPost')->name('createPost');
+            Route::post('/createComments','createComments')->name('createComments');
+            Route::put('/updatePost','updatePost')->name('updatePost');
+            Route::put('/updateComments','index')->name('updateComments');
+            Route::delete('/deletePost','deletePost')->name('deletePost');
+            Route::delete('/deleteComments','deleteComments')->name('deleteComments'); 
+        }); 
+    });
 });
+
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
